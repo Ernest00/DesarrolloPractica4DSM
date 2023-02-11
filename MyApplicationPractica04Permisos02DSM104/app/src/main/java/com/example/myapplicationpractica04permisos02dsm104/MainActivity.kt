253e8630e2e29ff239efe5f5d1b50f390b0e0f5a
Ernest00 <ernesto.anaya2014@gmail.com>
2023-02-10 18:31:11 -0600

    Add files Practica 4 DSM

---
+++ MyApplicationPractica04Permisos02DSM104/app/src/main/java/com/example/myapplicationpractica04permisos02dsm104/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.myapplicationpractica04permisos02dsm104
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private  val TAG = "Practica 04 - Permisos"
+    private val CODIGOSOLICITUDGRABAR =101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ConfigurarPermisos()
+    }
+
+    private fun ConfigurarPermisos(){
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            Log.i(TAG, "Permiso denegado para grabar")
+            if
+                    (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECORD_AUDIO)){
+                        val builder = AlertDialog.Builder(this)
+                builder.setMessage("Permiso para accerder al microfono es requerido por esta App para grabar audio.")
+                    .setTitle("Permission required")
+                builder.setPositiveButton("OK"){dialog, id -> Log.i(TAG, "Seleccionado")
+                hacerSolicitudPermiso()
+                }
+                val dialog = builder.create()
+                dialog.show()
+            }else{
+                hacerSolicitudPermiso()
+            }
+        }
+    }
+
+    private  fun hacerSolicitudPermiso(){
+        ActivityCompat.requestPermissions(this,
+        arrayOf(android.Manifest.permission.RECORD_AUDIO), CODIGOSOLICITUDGRABAR)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CODIGOSOLICITUDGRABAR -> {
+                if(grantResults.isEmpty() || grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED){
+                    Log.i(TAG, "Permiso a sido denegado por el usuario")
+                    Toast.makeText(applicationContext, "Permiso a sido denegado por el usuario", Toast.LENGTH_SHORT).show()
+                    /*Escribir codigo de Permiso Denegado, según se requiera*/
+                }else{
+                    Log.i(TAG, "Permiso concedido por el usuario")
+                    Toast.makeText(applicationContext, "Permiso concedido por el usuario", Toast.LENGTH_SHORT).show()
+                    /*Escribir codigo de Permiso Denegado, según se requiera*/
+                }
+            }
+        }
+    }
+}
